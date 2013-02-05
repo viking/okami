0b0818cd112505922d5c29b6eaea647348164cd7
Jeremy Stephens <jeremy.f.stephens@vanderbilt.edu>
2013-02-05 15:49:18 -0600

    Add filename to tracks

--- db/migrate/003_create_tracks.rb
+++ db/migrate/003_create_tracks.rb
@@ -4,6 +4,7 @@ Sequel.migration do
       primary_key :id
       String :name
       Integer :number
+      String :filename
       foreign_key :artist_id, :artists
       foreign_key :album_id, :albums
     end
