8f48be4447519158fbc419d803951253059a8d63
Jeremy Stephens <jeremy.f.stephens@vanderbilt.edu>
2013-02-07 16:47:31 -0600

    Add option to include tracks in album view

--- lib/playa/views/albums/index.rb
+++ lib/playa/views/albums/index.rb
@@ -2,7 +2,11 @@ module Playa
   module Views
     module Albums
       class Index < Mustache
-        attr_reader :albums
+        attr_reader :albums, :include_tracks
+
+        def tracks_hidden
+          !@include_tracks
+        end
       end
     end
   end
